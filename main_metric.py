8d3ca7eaa5557946b2905289dc0972c7b4e84b74
Kira Selby <kaselby@uwaterloo.ca>
2023-04-24 17:17:15 -0400

    run eval

--- main_metric.py
+++ main_metric.py
@@ -25,7 +25,8 @@ def fid(real, fake, gpu, batch_size=50, dims=2048):
     #os.system(command)
 
     device = torch.device(gpu)
-    fid = calculate_fid_given_paths(real, fake, batch_size=batch_size, device=device, dims=dims)
+    fid_score = calculate_fid_given_paths(real, fake, batch_size=batch_size, device=device, dims=dims)
+    return fid_score
 
 
 def LPIPS(root):
@@ -56,7 +57,48 @@ def LPIPS(root):
                 d = model(img1, img2, normalize=True)
                 temp.append(d.detach().cpu().numpy())
         res.append(np.mean(temp))
-    print(np.mean(res))
+    lpips_score = np.mean(res)
+    print(lpips_score)
+    return lpips_score
+
+
+
+def eval_scores(data, n_cond, trainer, real_dir, fake_dir, transform):
+    per = np.random.permutation(data.shape[1])
+    data = data[:, per, :, :, :]
+
+    num = n_cond
+    data_for_gen = data[:, :num, :, :, :]
+    data_for_fid = data[:, num:num+128, :, :, :]
+    if os.path.exists(real_dir):
+        for cls in tqdm(range(data_for_fid.shape[0]), desc='preparing real images'):
+            for i in range(128):
+                if data_for_fid.shape[1] < 128:
+                    idx = np.random.choice(data_for_fid.shape[1], 1).item()
+                else:
+                    idx = i
+                real_img = data_for_fid[cls, idx, :, :, :]
+                if args.dataset == 'vggface':
+                    real_img *= 255
+                real_img = Image.fromarray(np.uint8(real_img))
+                real_img.save(os.path.join(real_dir, '{}_{}.png'.format(cls, str(i).zfill(3))), 'png')
+
+    if os.path.exists(fake_dir):
+        for cls in tqdm(range(data_for_gen.shape[0]), desc='generating fake images'):
+            for i in range(128):
+                idx = np.random.choice(data_for_gen.shape[1], args.n_sample_test)
+                imgs = data_for_gen[cls, idx, :, :, :]
+                imgs = torch.cat([transform(img).unsqueeze(0) for img in imgs], dim=0).unsqueeze(0).cuda()
+                fake_x = trainer.generate(imgs)
+                output = unloader(fake_x[0].cpu())
+                output.save(os.path.join(fake_dir, '{}_{}.png'.format(cls, str(i).zfill(3))), 'png')
+
+    fid_score=fid(real_dir, fake_dir, int(args.gpu))
+    lpips_score=LPIPS(fake_dir)
+
+    shutil.rmtree(real_dir)
+    shutil.rmtree(fake_dir)
+    return fid_score, lpips_score
 
 
 parser = argparse.ArgumentParser()
@@ -65,9 +107,12 @@ parser.add_argument('--dataset', type=str)
 parser.add_argument('--real_dir', type=str)
 parser.add_argument('--fake_dir', type=str)
 parser.add_argument('--ckpt', type=str, default=None)
-parser.add_argument('--gpu', type=str, default=0)
+parser.add_argument('--gpu', type=str, default='0')
 parser.add_argument('--n_sample_test', type=int, default=3)
 parser.add_argument('--n_test', type=int, default=3)
+parser.add_argument('--n_cond', type=int, default=10)
+parser.add_argument('--n_exps', type=int, default=3)
+parser.add_argument('--eval_path', type=str, default="eval_results.txt")
 args = parser.parse_args()
 
 conf_file = os.path.join(args.name, 'configs.yaml')
@@ -101,64 +146,33 @@ if __name__ == '__main__':
     data = np.load(config['data_root'])
     if args.dataset == 'flower':
         data = data[85:]
-        num = 10
     elif args.dataset == 'animal':
         data = data[119:]
-        num = 10
     elif args.dataset == 'animal2':
         data = np.load("datasets/animal2.npy")
-        num = 10
     elif args.dataset == 'vggface':
         data = data[1802:]
-        num = 30
 
-    per = np.random.permutation(data.shape[1])
-    data = data[:, per, :, :, :]
+    trainer = Trainer(config)
+    if args.ckpt:
+        last_model_name = os.path.join(args.name, 'checkpoints', args.ckpt)
+    else:
+        last_model_name = get_model_list(os.path.join(args.name, 'checkpoints'), "gen")
+    trainer.load_ckpt(last_model_name)
+    trainer.cuda()
+    trainer.eval()
+    
 
-    data_for_gen = data[:, :num, :, :, :]
-    data_for_fid = data[:, num:num+128, :, :, :]
+    fid_scores = []
+    lpips_scores=[]
+    for i in range(args.n_exps):
+        fid_score, lpips_score = eval_scores(data, args.n_cond, trainer, real_dir, fake_dir, transform)
+        fid_scores.append(fid_score)
+        lpips_scores.append(lpips_score)
+    fid_out = sum(fid_scores) / len(fid_scores)
+    lpips_out = sum(lpips_scores) / len(lpips_scores)
 
-    # if os.path.exists(real_dir):
-    #     for cls in tqdm(range(data_for_fid.shape[0]), desc='preparing real images'):
-    #         for i in range(data_for_fid.shape[1]):
-    #             idx = i
-    #             real_img = data_for_fid[cls, idx, :, :, :]
-    #             if args.dataset == 'vggface':
-    #                 real_img *= 255
-    #             real_img = Image.fromarray(np.uint8(real_img))
-    #             real_img.save(os.path.join(real_dir, '{}_{}.png'.format(cls, str(i).zfill(3))), 'png')
-
-    # in case real images are less than 128
-    if os.path.exists(real_dir):
-        for cls in tqdm(range(data_for_fid.shape[0]), desc='preparing real images'):
-            for i in range(128):
-                if data_for_fid.shape[1] < 128:
-                    idx = np.random.choice(data_for_fid.shape[1], 1).item()
-                else:
-                    idx = i
-                real_img = data_for_fid[cls, idx, :, :, :]
-                if args.dataset == 'vggface':
-                    real_img *= 255
-                real_img = Image.fromarray(np.uint8(real_img))
-                real_img.save(os.path.join(real_dir, '{}_{}.png'.format(cls, str(i).zfill(3))), 'png')
-
-    if os.path.exists(fake_dir):
-        trainer = Trainer(config)
-        if args.ckpt:
-            last_model_name = os.path.join(args.name, 'checkpoints', args.ckpt)
-        else:
-            last_model_name = get_model_list(os.path.join(args.name, 'checkpoints'), "gen")
-        trainer.load_ckpt(last_model_name)
-        trainer.cuda()
-        trainer.eval()
-        for cls in tqdm(range(data_for_gen.shape[0]), desc='generating fake images'):
-            for i in range(128):
-                idx = np.random.choice(data_for_gen.shape[1], args.n_sample_test)
-                imgs = data_for_gen[cls, idx, :, :, :]
-                imgs = torch.cat([transform(img).unsqueeze(0) for img in imgs], dim=0).unsqueeze(0).cuda()
-                fake_x = trainer.generate(imgs)
-                output = unloader(fake_x[0].cpu())
-                output.save(os.path.join(fake_dir, '{}_{}.png'.format(cls, str(i).zfill(3))), 'png')
-
-    fid(real_dir, fake_dir, args.gpu)
-    LPIPS(fake_dir)
+    with open(args.eval_path, 'a') as writer:
+        fid_scores_str = ", ".join(["%.2f" % (x,) for x in fid_scores])
+        lpips_scores_str = ", ".join(["%.4f" % (x,) for x in lpips_scores])
+        writer.write("%s:\tFID: %.2f (%s)\tLPIPS: %.4f (%s)\n" % (args.dataset+"_"+args.n_cond, fid_out, fid_scores_str, lpips_out, lpips_scores_str)) 
---
+++ scripts/run_eval.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=setgan2eval
+#SBATCH --output=logs/slurm-%j.txt
+#SBATCH --open-mode=append
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=a40
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem=25GB
+#SBATCH --exclude=gpu109
+
+N_EXPS=3
+
+dataset=$1
+n=$2
+
+python main_metric.py --gpu 0 --dataset $dataset \
+--name results/${dataset}_lofgan \
+--real_dir datasets/for_fid/$dataset --ckpt gen_00100000.pt \
+--fake_dir test_for_fid --n_cond $n --n_exps $N_EXPS
